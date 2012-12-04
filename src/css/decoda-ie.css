f042a691d274e25717c3cd0eafd73e0ec49eec3e
Miles Johnson <mileswjohnson@gmail.com>
2012-12-04 15:26:47 -0800

    Updated docs

--- src/css/decoda-ie.css
+++ src/css/decoda-ie.css
@@ -1,6 +1,5 @@
 /**
- * @author      Miles Johnson - http://milesj.me
- * @copyright   Copyright 2006-2012, Miles Johnson, Inc.
+ * @copyright   Copyright 2006-2013, Miles Johnson - http://milesj.me
  * @license     http://opensource.org/licenses/mit-license.php - Licensed under The MIT License
  * @link        http://milesj.me/code/mootools/decoda
  */
