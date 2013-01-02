04adc7e1322e8821f5d1c6d3b2b461d648efe95e
Miles Johnson <mileswjohnson@gmail.com>
2013-01-02 01:06:35 -0800

    Added composer to handle dependencies

--- bin/package.php
+++ bin/package.php
@@ -1,62 +1,91 @@
 <?php
+/**
+ * @copyright	Copyright 2006-2013, Miles Johnson - http://milesj.me
+ * @license		http://opensource.org/licenses/mit-license.php - Licensed under the MIT License
+ * @link		http://milesj.me/code/mootools/decoda
+ */
 
-// Includes, no need for an auto-loader
-require_once 'cssmin.php';
-require_once 'jsmin/jsmin.php';
-require_once 'packager/Packager.php';
-require_once 'packager/Minifier.php';
-require_once 'packager/minifiers/JsMinifier.php';
-require_once 'packager/minifiers/CssMinifier.php';
+require_once '../vendor/autoload.php';
 
-use \mjohnson\packager\Packager;
-use \mjohnson\packager\minifiers\JsMinifier;
-use \mjohnson\packager\minifiers\CssMinifier;
+use Packager\Packager;
+use Packager\Minifier\JsMinifier;
+use Packager\Minifier\CssMinifier;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
-// Fetch variables
-$minify = isset($_GET['minify']) ? (bool) $_GET['minify'] : true;
-$zip = isset($_GET['zip']) ? (bool) $_GET['zip'] : false;
+define('VERSION', file_get_contents('../version.md'));
 
-$packager = new Packager(dirname(__DIR__));
+// Callback to execute packaging logic
+$callback = function(InputInterface $input, OutputInterface $output) {
+	$output->writeln(sprintf('<comment>MooTools Decoda %s</comment>', VERSION));
 
-if ($minify) {
-	$packager->addMinifier(new JsMinifier());
-	$packager->addMinifier(new CssMinifier());
-}
+	$packager = new Packager(dirname(__DIR__));
 
-// Package the contents
-if ($packager->package(array('Js'), array('outputFile' => 'bin/{name}-{version}.min.js'))) {
-	echo 'Javascript packaged<br>';
-} else {
-	echo 'Javascript failed to package<br>';
-}
-
-if ($packager->package(array('Css'), array('outputFile' => 'bin/{name}-{version}.min.css'))) {
-	echo 'CSS packaged<br>';
-} else {
-	echo 'CSS failed to package<br>';
-}
-
-if ($packager->package(array('Css.IE'), array('outputFile' => 'bin/{name}-ie-{version}.min.css'))) {
-	echo 'CSS.IE packaged<br>';
-} else {
-	echo 'CSS.IE failed to package<br>';
-}
-
-// Archive the output into a zip file
-if ($zip) {
-	$archive = array(
-		array('path' => 'bin/{name}-{version}.min.css', 'folder' => 'css/'),
-		array('path' => 'bin/{name}-ie-{version}.min.css', 'folder' => 'css/'),
-		array('path' => 'bin/{name}-{version}.min.js', 'folder' => 'js/'),
-		array('path' => 'src/img/icons-black.png', 'folder' => 'img/'),
-		array('path' => 'src/img/icons-white.png', 'folder' => 'img/'),
-		'license.md',
-		'readme.md'
-	);
-
-	if ($packager->archive('bin/{name}-{version}', $archive)) {
-		echo 'Contents archived';
-	} else {
-		echo 'Contents failed to archive';
+	// Add minifiers
+	if ($input->getOption('minify')) {
+		$packager->addMinifier(new JsMinifier());
+		$packager->addMinifier(new CssMinifier());
 	}
-}
+
+	// Package the contents
+	$output->writeln('');
+	$output->writeln('--- Packaging ---');
+
+	if ($packager->package(array('js'), array('outputFile' => 'bin/{name}-{version}.min.js'))) {
+		$output->writeln('<info>Javascript packaged</info>');
+	} else {
+		$output->writeln('<error>Javascript failed to package</error>');
+	}
+
+	if ($packager->package(array('css'), array('outputFile' => 'bin/{name}-{version}.min.css'))) {
+		$output->writeln('<info>CSS packaged</info>');
+	} else {
+		$output->writeln('<error>CSS failed to package</error>');
+	}
+
+	if ($packager->package(array('css/ie'), array('outputFile' => 'bin/{name}-ie-{version}.min.css'))) {
+		$output->writeln('<info>IE CSS packaged</info>');
+	} else {
+		$output->writeln('<error>IE CSS failed to package</error>');
+	}
+
+	// Archive the output
+	if ($outputFile = $input->getOption('archive')) {
+		$output->writeln('');
+		$output->writeln('--- Archiving ---');
+
+		$archive = array(
+			array('path' => 'bin/{name}-{version}.min.css', 'folder' => 'css/'),
+			array('path' => 'bin/{name}-ie-{version}.min.css', 'folder' => 'css/'),
+			array('path' => 'bin/{name}-{version}.min.js', 'folder' => 'js/'),
+			array('path' => 'src/img/icons-black.png', 'folder' => 'img/'),
+			array('path' => 'src/img/icons-white.png', 'folder' => 'img/'),
+			'license.md',
+			'readme.md',
+			'version.md'
+		);
+
+		if ($packager->archive($outputFile, $archive)) {
+			$output->writeln('<info>Contents archived</info>');
+		} else {
+			$output->writeln('<error>Contents failed to archive</error>');
+		}
+	}
+};
+
+// Start the console and run
+array_splice($_SERVER['argv'], 1, 0, 'package');
+
+$console = new Application('Decoda', VERSION);
+
+$console->register('package')
+	->setDescription('Compress and package the static assets')
+	->addOption('minify', null, InputOption::VALUE_OPTIONAL, 'Enable static asset minification', true)
+	->addOption('archive', null, InputOption::VALUE_OPTIONAL, 'Archive the packaged files into a single zip', 'bin/{name}-{version}')
+	->setCode($callback);
+
+$console->run();
