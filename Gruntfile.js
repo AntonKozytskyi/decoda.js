837055ed318e7b2092403c600118e6662a92992b
Miles Johnson <mileswjohnson@gmail.com>
2013-07-02 13:04:15 -0700

    Added cssmin to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,37 @@
 module.exports = function(grunt) {
+	function createBanner() {
+		return "/*!\n" +
+			" * <%= pkg.name %> v<%= pkg.version %>\n" +
+			" * <%= pkg.copyright %> - <%= pkg.homepage %>\n" +
+			" * <%= pkg.licenses[0].type %> - <%= pkg.licenses[0].url %>\n" +
+			" */";
+	}
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		name: '<%= pkg.name.toLowerCase() %>-<%= pkg.version %>',
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: createBanner() + "\n",
+				report: 'min'
 			},
 			build: {
 				src: 'src/js/*.js',
-				dest: 'build/<%= pkg.name %>-<%= pkg.version %>.min.js'
+				dest: 'build/<%= name %>.min.js'
+			}
+		},
+		cssmin: {
+			options: {
+				banner: createBanner(),
+				report: 'min'
+			},
+			build: {
+				files: {
+					'build/<%= name %>.min.css': 'src/css/decoda.css',
+					'build/decoda-ie-<%= pkg.version %>.min.css': 'src/css/decoda-ie.css',
+					'build/decoda-theme-<%= pkg.version %>.min.css': 'src/css/decoda-theme.css'
+				}
 			}
 		},
 		jshint: {
@@ -46,8 +68,9 @@ module.exports = function(grunt) {
 	// Load plugins
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	// Register tasks
 	grunt.registerTask('validate', ['jshint']);
-	grunt.registerTask('default', ['jshint', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
 };
